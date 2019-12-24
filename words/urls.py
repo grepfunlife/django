5e27ce70eee7f11b67435934c42f7b6254ef7644
polanskaya <polanskaya@yamoney.ru>
2019-12-24 22:51:57 +0300

    в

--- words/urls.py
+++ words/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_list, name='list_list'),
+    path('list/<int:pk>/', views.list_detail, name='list_detail'),
 ]
--- words/views.py
+++ words/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import List
 
 
 def list_list(request):
     lists = List.objects.all
     return render(request, 'words/list_list.html', {'lists': lists})
+
+def list_detail(request, pk):
+    list = get_object_or_404(List, pk=pk)
+    return render(request, 'words/list_detail.html', {'list': list})
