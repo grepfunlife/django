7025f98436daf366c3cfbe61839c459f27f0b97d
polanskaya <polanskaya@yamoney.ru>
2019-12-25 00:08:08 +0300

    в

--- words/urls.py
+++ words/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.list_list, name='list_list'),
     path('list/<int:pk>/', views.list_detail, name='list_detail'),
+    path('list/new/', views.list_new, name='list_new'),
+    path('list/<int:pk>/edit/', views.list_edit, name='list_edit'),
 ]
--- words/views.py
+++ words/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import List
+from .forms import ListForm
 
 
 def list_list(request):
     lists = List.objects.all
     return render(request, 'words/list_list.html', {'lists': lists})
 
+
 def list_detail(request, pk):
     list = get_object_or_404(List, pk=pk)
     return render(request, 'words/list_detail.html', {'list': list})
+
+
+def list_new(request):
+    if request.method == "POST":
+        form = ListForm(request.POST)
+        if form.is_valid():
+            list = form.save()
+            list.save()
+            return redirect('list_detail', pk=list.pk)
+    else:
+        form = ListForm()
+    return render(request, 'words/list_edit.html', {'form': form})
+
+
+def list_edit(request, pk):
+    list = get_object_or_404(List, pk=pk)
+    if request.method == "POST":
+        form = ListForm(request.POST, instance=list)
+        if form.is_valid():
+            list = form.save()
+            list.save()
+            return redirect('list_detail', pk=list.pk)
+    else:
+        form = ListForm(instance=list)
+    return render(request, 'words/list_edit.html', {'form': form})
